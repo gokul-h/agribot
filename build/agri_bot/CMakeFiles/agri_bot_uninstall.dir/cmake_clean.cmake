91c2e402f6b44a257096cac701ced82cf5f9df1b
Gokul H <72368205+gokul-h@users.noreply.github.com>
2023-09-23 23:07:11 +0530

    FIx

--- build/agri_bot/CMakeFiles/agri_bot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/agri_bot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/agri_bot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/py_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/gokul/Desktop/agribot_p1/install/py_pubsub'
